12678c66d7799885421e55ffef3fdb32a6b9bbeb
Your NameKevin <hello@yourpayroll.info>
2025-07-03 12:28:43 +0100

    added create org

--- src/App.js
+++ src/App.js
@@ -1105,6 +1105,7 @@ const TestInProgress = ({ user, testId, db, appId, onNavigate }) => {
     );
 };
 
+
 const TestFinishedScreen = ({ user, onNavigate }) => (
     <div className="text-center py-10 max-w-xl mx-auto">
         <svg className="w-16 h-16 mx-auto text-emerald-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
